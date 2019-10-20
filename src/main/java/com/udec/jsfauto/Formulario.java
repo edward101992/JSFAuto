c86e160a289d97fbc98b2099290980ee1f5e067b
Edward <edwardconw@gmail.com>
2019-10-19 21:22:07 -0500

    Documentacion 1

--- src/main/java/com/udec/jsfauto/Formulario.java
+++ src/main/java/com/udec/jsfauto/Formulario.java
@@ -5,6 +5,7 @@
  */
 package com.udec.jsfauto;
 
+import java.io.Serializable;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
@@ -18,11 +19,12 @@ import org.primefaces.event.SelectEvent;
 
 /**
  *
- * @author SuperUs
+ * @author Edward Ramos
+ * @author Victor Preciado
  */
 @Named(value = "formulario")
 @Dependent
-public class Formulario {
+public class Formulario implements Serializable{
 
     
     private String nombre;
@@ -30,48 +32,82 @@ public class Formulario {
     private List<String> marca = new ArrayList<String>();
     
     @PostConstruct
+    /**
+     * Metodo que llena la lista marca
+     */
     public void llenarMarca(){   
         marca.add("BMW");
         marca.add("Toyota");
+        marca.add("Ferrari");
+        marca.add("Nissan");
     }
-    
+    /**
+     * Metodo que muestra un mensaje con el añño que selecciono el usuario
+     * @param event variable que activa el mensaje
+     */
      public void onDateSelect(SelectEvent event) {
         FacesContext facesContext = FacesContext.getCurrentInstance();
         SimpleDateFormat format = new SimpleDateFormat("yyyy");
         facesContext.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Modelo seleccionado", format.format(event.getObject())));
     }
-     
+     /**
+      * Contructor de la clase Formulario
+      * @param nombre variable que identifica el nombre del usuario
+      * @param modelo variable que identifica el modelo del auto
+      * @param marca varibale que identifica la marca del auto
+      */
     public Formulario(String nombre, Date modelo,List<String> marca) {
         this.nombre = nombre;
         this.modelo = modelo;
         this.marca = marca;
     }
-
+    /**
+     * get de la variable nombre
+     * @return nombre
+     */
     public String getNombre() {
         return nombre;
     }
-
+    /**
+     * set de la variable nombre
+     * @param nombre 
+     */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
-
+    /**
+     * get de la variable modelo
+     * @return modelo
+     */
     public Date getModelo() {
         return modelo;
     }
-
+    /**
+     * set de la variable modelo
+     * @param modelo 
+     */
     public void setModelo(Date modelo) {
         this.modelo = modelo;
     }
-
+    /**
+     * get de la variable marca
+     * @return marca
+     */
     public List<String> getMarca() {
         return marca;
     }
-
+    /**
+     * set de la variable marca
+     * @param marca 
+     */
     public void setMarca(List<String> marca) {
         this.marca = marca;
     }
     
-   
+   /**
+    * Constructor de la clase Formulario
+    */
+    
     public Formulario() {
     }
     
