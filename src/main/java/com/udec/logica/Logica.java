030a32873f8017f8b651f811b24853b904c746de
Edward <edwardconw@gmail.com>
2019-10-19 19:31:57 -0500

    Creacion de vista y Clases

---
+++ src/main/java/com/udec/logica/Logica.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.udec.logica;
+
+import javax.inject.Named;
+import javax.enterprise.context.Dependent;
+
+/**
+ *
+ * @author SuperUs
+ */
+@Named(value = "logica")
+@Dependent
+public class Logica {
+
+    /**
+     * Creates a new instance of Logica
+     */
+    public Logica() {
+    }
+    
+}
