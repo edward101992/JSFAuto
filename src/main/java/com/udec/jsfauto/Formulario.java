8f755de0876a8499232f005dbb5174c4011f0da0
Edward <edwardconw@gmail.com>
2019-10-19 20:13:30 -0500

    Creacion de lista y variables

--- src/main/java/com/udec/jsfauto/Formulario.java
+++ src/main/java/com/udec/jsfauto/Formulario.java
@@ -5,6 +5,10 @@
  */
 package com.udec.jsfauto;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import javax.annotation.PostConstruct;
 import javax.inject.Named;
 import javax.enterprise.context.Dependent;
 
@@ -16,9 +20,49 @@ import javax.enterprise.context.Dependent;
 @Dependent
 public class Formulario {
 
-    /**
-     * Creates a new instance of Formulario
-     */
+    
+    private String nombre;
+    private Date modelo;
+    private List<String> marca = new ArrayList<String>();
+    
+    @PostConstruct
+    public void llenarMarca(){   
+        marca.add("BMW");
+        marca.add("Toyota");
+    }
+    
+    
+    public Formulario(String nombre, Date modelo,List<String> marca) {
+        this.nombre = nombre;
+        this.modelo = modelo;
+        this.marca = marca;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Date getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(Date modelo) {
+        this.modelo = modelo;
+    }
+
+    public List<String> getMarca() {
+        return marca;
+    }
+
+    public void setMarca(List<String> marca) {
+        this.marca = marca;
+    }
+    
+   
     public Formulario() {
     }
     
